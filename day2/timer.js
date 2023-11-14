296c8aadef89b123826b87c0e03e160282b5bcbb
haazzero <wwwjungha@gmail.com>
2023-11-14 17:09:50 +0900

    docs : 요구사항 정의 상세

--- day2/timer.js
+++ day2/timer.js
@@ -3,7 +3,9 @@ let timer;
 
 function startTimer() {
     //START 버튼 클릭 시 입력 영역 사라짐
-    document.getElementById("inputTimer").style.display ='none';		
+    document.getElementById("inputTimer").style.display ='none';
+    hour = document.querySelector("#startHour").value; 
+    if (hour === "") hour = 0;		
     min = document.querySelector("#startMin").value; 
     if (min === "") min = 0;
     sec = document.querySelector("#startSec").value;
@@ -11,28 +13,35 @@ function startTimer() {
     timer = setInterval(countTimer, 1000);
   }
 
-function countTimer() {
-if (sec != 0) {
-    sec--;
-    document.querySelector("#display").innerText =
-    min + "분" + sec + "초 남았습니다.";
-} else {
-    if (min != 0) {
-    min--;
-    sec = 59;
+  function countTimer() {
+    if (sec != 0) {
+        sec--;
+        document.querySelector("#display").innerText =
+        min + "분" + sec + "초 남았습니다.";
     } else {
-        resetTimer(timer, "타이머 종료");
+        if (min != 0) {
+            min--;
+            sec = 59;
+        } else {
+            resetTimer();
+            clearInterval(timer);
+            alert("타이머 종료");
+        }
     }
-}
 }
 
 function stopTimer() {
+    //STOP 버튼 클릭 시 카운트 다운 정지
+    clearInterval(timer);
   }
 
 function resetTimer() {
      //RESET 버튼 클릭 시 입력 영역 나타남
     document.getElementById("inputTimer").style.display ='block';	
-    
+
+    document.querySelector("#display").innerText =
+    hour + ":" min + ":" + sec
+
     document.getElementById("startHour").value = "";
     document.getElementById("startMin").value = "";
     document.getElementById("startSec").value = "";
