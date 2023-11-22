e3d368fe9324f654534a02726cbff20ea5bba94f
haazzero <wwwjungha@gmail.com>
2023-11-22 11:53:09 +0900

    feat : 슬라이드 태그 텍스트 추가

--- day8/js/data/card.js
+++ day8/js/data/card.js
@@ -1,7 +1,7 @@
 const intern = [
     {
       category: "채용형",
-      dDay: "D-8",
+      dDay: "D-1",
       job: "기획·비즈니스",
       title:
         "글로벌 시대에 맞춰 전세계 누구나 쉽게 즐길 수 있는 영상(숏폼) 콘텐츠 기획",
@@ -9,7 +9,7 @@ const intern = [
     },
     {
       category: "교육형",
-      dDay: "D-11",
+      dDay: "D-1",
       job: "마케팅",
       title:
         "펀딩 프로젝트 성공을 위한 구름맨션 브랜드 인지도 향상 SNS 마케팅 콘텐츠 기획",
@@ -17,28 +17,28 @@ const intern = [
     },
     {
       category: "채용형",
-      dDay: "D-8",
+      dDay: "D-1",
       job: "마케팅",
       title: "빈크런치 브랜드 및 제품을 효과적으로 알리기 위한 SNS 콘텐츠 제작",
       img: "./img/card_intern_3.png",
     },
     {
       category: "교육형",
-      dDay: "D-2",
+      dDay: "D-1",
       job: "마케팅",
       title: "유튜브 쇼핑 채널을 효과적으로 홍보할 수 있는 마케팅 방안 제안",
       img: "./img/card_intern_4.png",
     },
     {
       category: "교육형",
-      dDay: "D-10",
+      dDay: "D-1",
       job: "디자인",
       title: "주얼리 커머스 플랫폼 및 브랜드 디자인 컨셉안 기획",
       img: "./img/card_intern_5.png",
     },
     {
       category: "채용형",
-      dDay: "D-5",
+      dDay: "D-1",
       job: "기획·비즈니스",
       title: "기능성 식품 브랜드 '레디블룸' 신규 타겟 확장 방안 기획",
       img: "./img/card_intern_6.png",
